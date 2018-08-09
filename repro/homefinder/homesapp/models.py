01437e0e1753754cb917519925b208584d627c07
buikevin96 <buikevin96@gmail.com>
2018-08-09 10:37:15 -0400

    Created the superuser

--- repro/homefinder/homesapp/models.py
+++ repro/homefinder/homesapp/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+# Location Model which will inherit from models.Model
 class Location(models.Model):
     location_name = models.CharField(max_length = 200) # Name of location
     location_type = models.CharField(max_length = 200) # Type of location
+
+    def __str__(self):
+        return self.location_name
 
 class Property(models.Model):
     location = models.ForeignKey(Location, on_delete = models.CASCADE)
     property_name = models.CharField(max_length = 200)
     area = models.CharField(max_length = 200)
 
-    
+    def __str__(self):
+        return self.property_name
