88ce9aa7b169def92d22a32cfdb39b7797a46b70
buikevin96 <buikevin96@gmail.com>
2018-08-09 00:03:34 -0400

    Started creating the models

--- repro/homefinder/homesapp/models.py
+++ repro/homefinder/homesapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 200) # Name of location
+    location_type = models.CharField(max_length = 200) # Type of location
+
+class Property(models.Model):
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    property_name = models.CharField(max_length = 200)
+    area = models.CharField(max_length = 200)
+
+    
